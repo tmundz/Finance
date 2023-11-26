bd922aba908b6b5d309ec3cd18e7f2295cc5424d
Travis <mundyt4@mymacewan.ca>
2023-11-26 00:15:19 -0700

    Add redux and start the server

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import "@/index.css"
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import {api } from './state/api';
 
+export const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
+})
+setupListeners(store.dispatch);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
